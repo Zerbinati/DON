00e271ae0bde2e61c2d3c0f9832107d576a8f342
Ehsan Rashid <ehsan.rashid@gmail.com>
2014-09-13 01:08:55 +0500

    merged master-ltc

--- src/Searcher.cpp
+++ src/Searcher.cpp
@@ -1467,7 +1467,7 @@ namespace Search {
                         {
                             window[0] *= 1.250f;
                             bound [0] = max (best_value - window[0], -VALUE_INFINITE);
-                            if (window[1] > 1) window[1] *= 0.935f;
+                            if (window[1] > 1) window[1] *= 0.955f;
                             bound [1] = min (best_value + window[1], +VALUE_INFINITE);
 
                             Signals.root_failedlow = true;
@@ -1478,7 +1478,7 @@ namespace Search {
                         {
                             window[1] *= 1.250f;
                             bound [1] = min (best_value + window[1], +VALUE_INFINITE);
-                            if (window[0] > 1) window[0] *= 0.935f;
+                            if (window[0] > 1) window[0] *= 0.955f;
                             bound [0] = max (best_value - window[0], -VALUE_INFINITE);
                         }
                         else
