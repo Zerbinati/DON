cd5deb16fcd41272923c082c1b13130667edebe0
Ehsan Rashid <ehsan.rashid@gmail.com>
2017-07-19 16:56:19 +0500

    refactor

--- src/Transposition.h
+++ src/Transposition.h
@@ -57,7 +57,7 @@ namespace Transposition {
         u08   generation () const { return u08  (gb08 & 0xFC); }
         bool  empty      () const { return d08 == Empty; }
 
-        // The worth of an entry is calculated as its depth minus 8 times its relative age.
+        // The worth of an entry is calculated as its depth minus 2 times its relative age.
         // Due to packed storage format for generation and its cyclic nature
         // add 0x103 (0x100 + 0x003 (BOUND_EXACT) to keep the lowest two bound bits from affecting the result)
         // to calculate the entry age correctly even after generation overflows into the next cycle.
@@ -82,8 +82,7 @@ namespace Transposition {
             }
             if (   ee
                 || k16 != key16
-                || (   MOVE_NONE != m
-                    && m16 != m))
+                || MOVE_NONE != m)
             {
                 k16 = key16;
                 m16 = u16(m);
