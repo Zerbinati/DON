92cf5fcb7ed6f408cb89c55decd8f9bb9d1c805a
Ehsan Rashid <ehsan.rashid@gmail.com>
2018-05-22 05:46:26 +0500

    refactor

--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -675,7 +675,8 @@ namespace {
         Bitboard kf_bb = KingFlank_bb[_file (fk_sq)];
 
         // Penalty for king on a pawn less flank
-        if (0 == (pos.pieces (PAWN) & kf_bb))
+        if (   0 != pos.pieces(Opp, ROOK, QUEN)
+            && 0 == (pos.pieces (PAWN) & kf_bb))
         {
             score -= PawnLessFlank;
         }
